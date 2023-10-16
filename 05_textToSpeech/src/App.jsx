d340d83074d4954edee69c8256901d80aa5bbebe
Anchal <anchalpaswan10@gmail.com>
2023-10-16 16:42:43 +0530

    added text-to-speech

---
+++ 05_textToSpeech/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
+function App() {
+  const [quote, setQuote] = useState("");
+  const [speechRate, setSpeechRate] = useState(1);
+  const [refresh, setRefresh] = useState(false);
+  const speech = new SpeechSynthesisUtterance();
+  const quoteURL = 'https://api.quotable.io/random';
+
+  useEffect(() => {
+    axios
+      .get(quoteURL)
+      .then((res) => {
+        const data = res.data;
+        setQuote(data.content);
+      });
+  }, [refresh]);
+
+  const speechHandler = () => {
+    speech.text = quote;
+    speech.rate = parseFloat(speechRate); // Set the speech rate
+    window.speechSynthesis.speak(speech);
+  }
+
+  const stopSpeechHandler = () => {
+    window.speechSynthesis.cancel();
+  }
+
+  const quoteHandler = () => {
+    setRefresh(!refresh);
+  }
+
+  return (
+    <div className='App'>
+      <button onClick={quoteHandler}>Generate Quote</button>
+   
+      <div className="quote">
+        <h2>{quote}</h2>
+      </div>
+      <button onClick={speechHandler}>SPEAK</button>
+      <button onClick={stopSpeechHandler}>Stop</button>
+      <select onChange={(e) => setSpeechRate(+e.target.value)}>
+        <option value="0.2">0.2x</option>
+        <option value="0.4">0.4x</option>
+        <option value="0.5">0.5x</option>
+        <option value="1">Normal</option>
+      </select>
+    </div>
+  );
+}
+
+export default App;
